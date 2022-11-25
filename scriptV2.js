ab2dbae5e7f79dc5f83d6014f50e67f514845675
Bryn M <betamu138@gmail.com>
2022-11-24 22:19:18 -0500

    Added comments to ScriptV2

--- scriptV2.js
+++ scriptV2.js
@@ -1,15 +1,15 @@
 function() {
-    var fullCS = prompt("CS#? Add \".T\" or use \"REC.ACCT T\" for termed accts.");
+    var fullCS = prompt("CS#? Add \".T\" or use \"REC.ACCT T\" for termed accts."); // Get CS# + termed status
     try {
-        var csArray = fullCS.split(/\.|\s/);
-    } catch (typeError) {
-        alert("Cancelled.");
+        var csArray = fullCS.split(/\.|\s/); // Split into individual values
+    } catch (typeError) { // If cancelled or no input
+        alert("Cancelled."); // End script
         return;
     }
-    var csRec = document.getElementById("ctl00_responsiveBody_txtCsReceiver");
+    var csRec = document.getElementById("ctl00_responsiveBody_txtCsReceiver"); // Set receiver val
     csRec.value = csArray[0];
-    var csAcct = document.getElementById("ctl00_responsiveBody_txtCsAccount");
+    var csAcct = document.getElementById("ctl00_responsiveBody_txtCsAccount"); // Set Acct# val
     csAcct.value = csArray[1];
-    document.getElementById("ctl00_responsiveBody_chkIncludeTerminated").checked = (csArray.length = 3 && /t|T/.test(csArray[2]));
-    document.getElementById("ctl00_responsiveBody_btnSearch").click();
+    document.getElementById("ctl00_responsiveBody_chkIncludeTerminated").checked = (csArray.length = 3 && /t|T/.test(csArray[2])); // If "T" modifier added, check termed box
+    document.getElementById("ctl00_responsiveBody_btnSearch").click(); // Click "Search"
 }();
